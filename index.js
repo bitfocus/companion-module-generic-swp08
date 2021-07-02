1e2f42868f495f8f6ad74ddd6113d5ce0ee02e45
peter <mail@pjdaniel.org.uk>
2021-07-02 15:37:11 +0000

    remove old code

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ instance.prototype.setupVariables = function () {
 	self.setVariable('Source', self.selected_source)
 }
 
-instance.prototype.updateVariableDefinitions = function () {
+instance.prototype.updateVariableDefinitions = function() {
 	var self = this
 	const coreVariables = []
 
@@ -112,23 +112,27 @@ instance.prototype.updateVariableDefinitions = function () {
 		{
 			label: 'Selected source',
 			name: 'Source',
-		}
+		},
 	)
-
-	for (var i = 1; i <= Object.keys(self.source_names).length; i++) {
-		coreVariables.push({
-			label: 'Source ' + i.toString(),
-			name: 'Source_' + i.toString(),
-		})
+	
+	for (var i = 1; i <= Object.keys(self.source_names).length; i ++) {
+		coreVariables.push(
+			{
+				label: 'Source ' + i.toString(),
+				name: 'Source_' + i.toString(),
+			}
+		)
 	}
-
-	for (var i = 1; i <= Object.keys(self.dest_names).length; i++) {
-		coreVariables.push({
-			label: 'Destination ' + i.toString(),
-			name: 'Destination_' + i.toString(),
-		})
+	
+	for (var i = 1; i <= Object.keys(self.dest_names).length; i ++) {
+		coreVariables.push(
+			{
+				label: 'Destination ' + i.toString(),
+				name: 'Destination_' + i.toString(),
+			}
+		)
 	}
-
+	
 	self.setVariableDefinitions(coreVariables)
 	console.log(coreVariables)
 }
@@ -247,16 +251,13 @@ instance.prototype.init_tcp = function () {
 										l = l + 1
 										label_number = label_number + 1
 
-										self.SourceVariables = []
-										self.DestVariables = []
-
 										if (data[2] == 0x6a) {
 											// sources
 											self.source_names.splice(label_number - 1, 0, {
 												id: label_number,
 												label: label_number.toString() + ': ' + label.trim(),
 											})
-
+											
 											// self.setVariable('Source ' +  label_number.toString(), label.trim())
 										} else if (data[2] == 0x6b) {
 											// destinations
@@ -276,7 +277,7 @@ instance.prototype.init_tcp = function () {
 									// need to find a way of only calling these functions on the last part of the labels
 									self.updateVariableDefinitions()
 									self.updateVariableLabels()
-
+									
 									console.log(self.source_names)
 									console.log(self.dest_names)
 
@@ -313,6 +314,7 @@ instance.prototype.updateVariableLabels = function () {
 	for (var i = 0; i < Object.keys(self.dest_names).length; i++) {
 		self.setVariable('Destination_' + self.dest_names[i].id, self.dest_names[i].label)
 	}
+
 }
 instance.prototype.crosspointConnected = function (data) {
 	var self = this
